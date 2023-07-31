86411144c38744f1bf518d30962542747a35819f
tukky-14 <bigoonage0104@gmail.com>
2023-07-31 21:40:51 +0900

    戻るボタンを作成

---
+++ src/components/BackButton.tsx
@@ -0,0 +1,16 @@
+import Link from 'next/link';
+
+const BackButton = () => {
+    return (
+        <div className="w-full text-center">
+            <Link
+                href="/"
+                className="inline-flex items-center px-6 py-2 text-gray-500 bg-gray-100 rounded-md hover:bg-gray-200 hover:text-gray-600"
+            >
+                戻る
+            </Link>
+        </div>
+    );
+};
+
+export default BackButton;
--- src/pages/holiday/index.tsx
+++ src/pages/holiday/index.tsx
@@ -1,4 +1,5 @@
 import Header from '@/components/Header';
+import BackButton from '@/components/BackButton';
 
 export default function Home() {
     return (
@@ -6,6 +7,7 @@ export default function Home() {
             <div className="bg-white py-6 sm:py-8 lg:py-12">
                 <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
                     <Header title="祝日" />
+                    <BackButton />
                 </div>
             </div>
         </>
