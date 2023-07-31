8d217d4425c7280567b00055a828b2b83e762ed1
tukky-14 <bigoonage0104@gmail.com>
2023-07-31 21:36:35 +0900

    ヘッダーコンポーネントを修正

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
-const Header = () => {
+type Props = {
+    title: string;
+};
+
+const Header = (props: Props) => {
+    const { title } = props;
+
     return (
         <h2 className="mb-8 text-center text-2xl font-bold text-gray-800 md:mb-12 lg:text-3xl">
-            仕事でよく使うやつ
+            {title}
         </h2>
     );
 };
