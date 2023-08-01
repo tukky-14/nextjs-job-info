690559fa855979f7789ab7607dd57c2e6f31b080
tukky-14 <bigoonage0104@gmail.com>
2023-08-01 22:41:59 +0900

    Headタグを修正

--- src/components/CommonHead.tsx
+++ src/components/CommonHead.tsx
@@ -3,7 +3,7 @@ import Head from 'next/head';
 const CommonHead = () => {
     return (
         <Head>
-            <title>Job Info</title>
+            <title>仕事でよく使うやつ</title>
             <meta name="description" content="主に開発の仕事で使える情報をまとめました。" />
             <meta name="viewport" content="width=device-width, initial-scale=1" />
             <link rel="icon" href="/favicon.ico" />
