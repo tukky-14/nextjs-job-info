452a026a75e3ec9e3cc7539fef7bcb141af90269
tukky-14 <bigoonage0104@gmail.com>
2023-08-03 23:17:17 +0900

    祝日のデータ取得処理を追加

--- src/pages/holiday/index.tsx
+++ src/pages/holiday/index.tsx
@@ -1,7 +1,18 @@
 import Header from '@/components/Header';
 import BackButton from '@/components/BackButton';
+import { use, useEffect, useState } from 'react';
+import axios from 'axios';
 
 export default function Holiday() {
+    const [holidays, setHolidays] = useState({});
+
+    useEffect(() => {
+        (async () => {
+            const { data } = await axios.get('https://holidays-jp.github.io/api/v1/date.json');
+            console.log('data:', data);
+        })();
+    });
+
     return (
         <>
             <div className="bg-white py-6 sm:py-8 lg:py-12">
