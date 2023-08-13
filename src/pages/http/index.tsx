49950461bb8fd81871957aa395dcc1c99de5fa74
tukky-14 <bigoonage0104@gmail.com>
2023-08-13 10:18:16 +0900

    HTTPステータスコードを取得する処理を追加

--- src/pages/http/index.tsx
+++ src/pages/http/index.tsx
@@ -1,15 +1,43 @@
-import Header from '@/components/Header';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import BackButton from '@/components/BackButton';
+import Header from '@/components/Header';
+import Container from '@/components/Container';
+
+type HttpCode = {
+    code: number;
+    message: string;
+};
 
 export default function Http() {
+    const [httpCodes, setHttpCodes] = useState<HttpCode[]>([]);
+
+    useEffect(() => {
+        (async () => {
+            const { data } = await axios.get('/http.json');
+            setHttpCodes(data);
+        })();
+    }, []);
+
     return (
-        <>
-            <div className="bg-white py-6 sm:py-8 lg:py-12">
-                <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
-                    <Header title="HTTPステータスコード" />
-                    <BackButton />
-                </div>
+        <Container>
+            <Header title="HTTPステータスコード" />
+            <div className="flex items-center font-bold">
+                <div className="w-16 text-center">コード</div>
+                <div className="ml-2">説明</div>
             </div>
-        </>
+            {httpCodes.map((httpCode) => (
+                <div
+                    key={httpCode.code}
+                    className="flex items-center justify-between border-b border-gray-200 py-4 text-sm sm:text-base"
+                >
+                    <div className="flex items-center">
+                        <div className="w-16 text-center font-medium">{httpCode.code}</div>
+                        <div className="flex-1 ml-2">{httpCode.message}</div>
+                    </div>
+                </div>
+            ))}
+            <BackButton />
+        </Container>
     );
 }
