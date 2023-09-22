fecefd7d33f37851df0a9192ae290c90306be734
tukky-14 <bigoonage0104@gmail.com>
2023-09-22 23:51:10 +0900

    ヘッダーを調整

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,7 +8,7 @@ const Header = (props: Props) => {
     const { title } = props;
 
     return (
-        <h2 className="mb-6 text-center text-xl font-bold text-gray-600 lg:text-2xl">{title}</h2>
+        <h2 className="mb-10 text-center text-xl font-bold text-gray-600 lg:text-2xl">{title}</h2>
     );
 };
 
