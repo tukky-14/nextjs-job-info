937e877d46583e609437c769c8b564cfe54d28af
tukky-14 <bigoonage0104@gmail.com>
2023-09-28 21:15:25 +0900

    文字エンコーディングページの幅を調整

--- src/pages/encoding/index.tsx
+++ src/pages/encoding/index.tsx
@@ -23,7 +23,7 @@ export default function Encoding() {
         <Container>
             <Header title="文字エンコーディング" />
             <div className="pb-1 flex items-center font-bold border-b border-gray-800">
-                <div className="w-60">文字エンコーディング</div>
+                <div className="w-40 sm:w-60">文字エンコーディング</div>
                 <div className="flex-1 ml-2 w-full sm:text-left">説明</div>
             </div>
             <div className="h-96 overflow-scroll">
@@ -33,7 +33,7 @@ export default function Encoding() {
                         className="flex items-center justify-between border-b border-gray-200 py-4 text-sm sm:text-base"
                     >
                         <div className="flex items-center">
-                            <div className="w-60 font-medium">{httpCode.encoding}</div>
+                            <div className="w-40 sm:w-60 font-medium">{httpCode.encoding}</div>
                             <div className="flex-1 ml-2">{httpCode.description}</div>
                         </div>
                     </div>
